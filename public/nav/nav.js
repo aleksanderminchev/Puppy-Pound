1152bf5c489c9efa274c8861825e4204944e7f41
Alexander Minchev <walq286@gmail.com>
2021-06-17 15:00:11 +0200

    Async for fetch

--- public/nav/nav.js
+++ public/nav/nav.js
@@ -8,34 +8,35 @@ async function openForm() {
  async function logOut(){
     location.assign("/logout");
  };
-        
+         
 $(document).ready(function(){
-  console.log("REACHED FIRST");
+  // console.log("REACHED FIRST");
   $("#adoption_post").hide();
   $("#profile").hide();
   $("#logout").hide();
   $("#chathistory").hide();
 });
 const urlCorrect="/correct";
-        fetch(urlCorrect).then(response => response.json())
-        .then( (result) => {
-          console.log("REACHED SECND");
-          if(result.success){
-            $("#adoption_post").show();
-            $("#profile").show();
-            $("#logout").show();
-            $("#register").hide();
-            $("#loginButton").hide();
-            $("#chathistory").show();
-          }else{
-            $("#adoption_post").hide();
-            $("#profile").hide();
-            $("#logout").hide();
-            $("#register").show();
-            $("#loginButton").show();
-            $("#chathistory").hide();
-          }
+(async function (){
+ await fetch(urlCorrect).then(response => response.json())
+  .then( (result) => {
+    // console.log("REACHED SECND");
+    if(result.success){
+      $("#adoption_post").show();
+      $("#profile").show();
+      $("#logout").show();
+      $("#register").hide();
+      $("#loginButton").hide();
+      $("#chathistory").show();
+    }else{
+      $("#adoption_post").hide();
+      $("#profile").hide();
+      $("#logout").hide();
+      $("#register").show();
+      $("#loginButton").show();
+      $("#chathistory").hide();
+    }
 
-        }).catch(error => console.log('error:', error));
-
+  }).catch(error => console.log('error:', error));
+})();
 
